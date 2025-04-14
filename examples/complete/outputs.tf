c3d1cbdda9833df79fb43212b03c9de1adb555ce
shanye <shanye.xzq@alibaba-inc.com>
2025-04-14 15:52:58 +0800

    examples/complete: improve the examples

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,6 +1,8 @@
 output "this_redis_instance_id" {
-  value = module.example.this_redis_instance_id
+  description = "redis instance id"
+  value       = module.example.this_redis_instance_id
 }
 output "this_alicloud_ons_instance_id" {
-  value = module.example.this_alicloud_ons_instance_id
+  description = "ons instance id"
+  value       = module.example.this_alicloud_ons_instance_id
 }
